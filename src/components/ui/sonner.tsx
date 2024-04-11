373934a91f457a17050109bbec85737ce1564147
Gabriel T. Marques <gabrieltmm@gmail.com>
2024-04-11 09:42:17 -0300

    fixed components's eslint errors from shadcn/ui

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,10 +1,11 @@
-import { useTheme } from "next-themes"
-import { Toaster as Sonner } from "sonner"
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
@@ -23,7 +24,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
